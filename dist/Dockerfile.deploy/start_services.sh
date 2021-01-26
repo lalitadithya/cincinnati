db2bcce5c7bd319f216923ea2e28a7fd5af69d42
lalit@lalitadithya.com <lalit@lalitadithya.com>
2021-01-26 20:16:37 +0530

    Add ability to customize port

--- dist/Dockerfile.deploy/start_services.sh
+++ dist/Dockerfile.deploy/start_services.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 graph-builder --service.address 0.0.0.0 --upstream.registry.url "$REGISTRY" --upstream.registry.repository "$REPOSITORY" &
-policy-engine --service.address 0.0.0.0
+policy-engine --service.address 0.0.0.0 --service.port "$PORT"
